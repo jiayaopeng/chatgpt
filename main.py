253599b8069440a00c2e77cbc713449debc66ff8
Jiayao Peng <jiayao.peng01@gmail.com>
2023-07-20 22:17:06 +0100

    add gradio code as well as fix for the gradio

--- main.py
+++ main.py
@@ -2,63 +2,76 @@ import requests
 import openai
 import os
 import gradio as gr
-from bs4 import BeautifulSoup
 from langchain.indexes import VectorstoreIndexCreator
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.prompts import PromptTemplate
 from langchain.prompts import (
-    ChatPromptTemplate,
     PromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
 )
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+
 from langchain.chat_models import ChatOpenAI
 
 openai.api = os.environ["OPENAI_API_KEY"]
 
-
-
 if __name__ == '__main__':
-
     loader = DirectoryLoader('data/', glob="**/*.txt")
     docs = loader.load()
     embedding = OpenAIEmbeddings()
     len(docs)
-    if not os.path.exists("./chroma_db"): # create index from the docs
+    if not os.path.exists("./chroma"):  # create index from the docs
         print("Create index from loader....")
         index = VectorstoreIndexCreator().from_loaders([loader])
     else:
         print("Reuse the created index")
-        vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
+        vectorstore = Chroma(persist_directory="./chroma", embedding_function=embedding)
         index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 
     # test if the information can be retrieved
     question = "What is the job?"
     index.query(question)
 
-    prompt_template = """You are a interviewer from TUV Sud, and your candidate is interviewing for the OpenAI expert/Data Scientist Position
-    Please ask some data science question to the interviewee and also ask some questions which is specifically required for the job.
-    {context}
-    Interviewer: {question}
-    Interviewee:"""
-    prompt = PromptTemplate(template=prompt_template, input_variables=['context',"question"])
+    prompt_template = """You are a interviewer from TUV Sud, and you are looking for a candidate for the OpenAI expert/Data Scientist Position
+        at TUV SUD. Please ask some data science question to the interviewee and also ask some questions which is specifically required for the job.
+        {context}
+        Interviewer(You): {question}
+        Interviewee(The Human):"""
+    prompt = PromptTemplate(template=prompt_template, input_variables=['context', "question"])
 
     # last chain, chat over doc with history
     qa_chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0.8),
         retriever=index.vectorstore.as_retriever(),
-        combine_docs_chain_kwargs = {"prompt": prompt}
+        combine_docs_chain_kwargs={"prompt": prompt}
     )
+
+    with gr.Blocks() as demo:
+        gr.Markdown("## InterviewFreund")
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+        chat_history = []
+
+
+        def user(user_message, history):
+            print("Type of use msg:", type(user_message))
+            chat_history_tuples = []
+            for message in chat_history:
+                chat_history_tuples.append((message[0], message[1]))
+            response = qa_chain({"question": user_message, "chat_history": chat_history_tuples})
+            history.append((user_message, response["answer"]))
+            print(history)
+            return gr.update(value=""), history
+
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False)
+        clear.click(lambda: None, None, chatbot, queue=False)
+
+    demo.launch(debug=True)
+
+
 
 
 
